cf57dbf05d98fa11e05c5f80e52c6844d8976bfb
david.ahmed <david.a@pigeonline.io>
2019-08-31 15:01:42 +0400

    Serving all static files

--- interface/urls.py
+++ interface/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('consent', views.consent, name='consent')
+    path('consent', views.consent, name='consent'),
+    path('login', views.login, name='login'),
+    path('choices', views.choices, name='choices'),
+    path('sorry', views.sorry, name='sorry'),
+    path('success', views.success, name='success')
 ]
--- interface/views.py
+++ interface/views.py
@@ -12,3 +12,19 @@ def index(request):
 def consent(request):
     template = loader.get_template('interface/consent.html')
     return HttpResponse(template.render())
+
+def login(request):
+    template = loader.get_template('interface/login.html')
+    return HttpResponse(template.render())
+
+def sorry(request):
+    template = loader.get_template('interface/sorry.html')
+    return HttpResponse(template.render())
+
+def success(request):
+    template = loader.get_template('interface/success.html')
+    return HttpResponse(template.render())
+
+def choices(request):
+    template = loader.get_template('interface/choices.html')
+    return render(request, 'interface/choices.html', {'images':['img/1.jpg', 'img/2.jpg']})
