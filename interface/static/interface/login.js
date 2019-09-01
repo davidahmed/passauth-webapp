8f737f876c70ae2cc8ed3164a679cc9fa0c849ac
david.ahmed <david.a@pigeonline.io>
2019-09-01 13:51:31 +0400

    fixed javascript to be cross-compatible TESTED: safari, chrome, firefox, opera

---
+++ interface/static/interface/login.js
@@ -0,0 +1,81 @@
+"use strict";
+document.onmousemove = handleMouseMove;
+
+
+
+var hashCode = function(s){
+    return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
+}
+
+function handleMouseMove(event)
+{
+    var dot, eventDoc, doc, body, pageX, pageY;
+    event = event || window.event; // IE-ism
+    // If pageX/Y aren't available and clientX/Y
+    // are, calculate pageX/Y - logic taken from jQuery
+        // Calculate pageX/Y if missing and clientX/Y available
+    if (event.pageX == null && event.clientX != null) {
+        eventDoc = (event.target && event.target.ownerDocument) || document;
+        doc = eventDoc.documentElement;
+        body = eventDoc.body;
+        event.pageX = event.clientX +
+          (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+          (doc && doc.clientLeft || body && body.clientLeft || 0);
+        event.pageY = event.clientY +
+    (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+    (doc && doc.clientTop  || body && body.clientTop  || 0 );
+    }
+    //mouseMovements.push([event.pageX, event.pageY, event.timeStamp])
+    mouseMovements.push([event.pageX, event.pageY, $.now()])
+}
+
+function attachDurationHandler(element, arr){
+    var totalTime = {};
+    var pressed = {};
+
+    element.onkeydown = function(e) {
+        if (e.which in pressed) return;
+        pressed[e.which] = $.now();
+    };
+    element.onkeyup = function(e) {
+
+        if (!(e.which in pressed)){return;}
+
+        var duration = $.now() - pressed[e.which];
+
+        if (!(e.which in totalTime)) totalTime[e.which] = 0;
+        totalTime[e.which] += duration;
+        console.log('Key ' + e.which + ' pressed for ' + duration);
+        arr.push({'timestamp': pressed[e.which], 'duration': duration})
+        delete pressed[e.which];
+    };
+};
+
+var mouseMovements;
+var usernameFieldLogs;
+var passwordFieldLogs;
+
+var usernameField;
+var passwordField;
+var printLogs;
+
+window.onload = () => {
+    mouseMovements = new Array();
+    usernameFieldLogs = new Array();
+    passwordFieldLogs = new Array();
+    usernameField = document.getElementById('inputUsername');
+    passwordField = document.getElementById('inputPassword');
+
+    attachDurationHandler(document.getElementById('inputUsername'), usernameFieldLogs);
+    attachDurationHandler(document.getElementById('inputPassword'), passwordFieldLogs);
+};
+
+var printLogs = function() {
+    var username = document.getElementById('inputUsername').value;
+    var pass = document.getElementById('inputPassword').value;
+    console.log(username);
+    console.log(pass);
+    console.log(mouseMovements);
+    console.log(usernameFieldLogs);
+    console.log(passwordFieldLogs);
+};
--- interface/views.py
+++ interface/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.template import loader
-
+from django.views.decorators.csrf import csrf_exempt
 
 from django.http import HttpResponse
 
@@ -10,12 +10,18 @@ def index(request):
     return HttpResponse(template.render())
 
 def consent(request):
+    if request.session.get('consent', False):
+        return redirect('login')
+    request.session['consent'] = True
+
     template = loader.get_template('interface/consent.html')
     return HttpResponse(template.render())
 
+@csrf_exempt
 def login(request):
-    template = loader.get_template('interface/login.html')
-    return HttpResponse(template.render())
+    if request.POST:
+        print(list(request.POST.items()))
+    return render(request, 'interface/login.html')
 
 def sorry(request):
     template = loader.get_template('interface/sorry.html')
